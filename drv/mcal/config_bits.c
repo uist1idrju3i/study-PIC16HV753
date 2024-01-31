c92274edb3ef453cfaefa21041d856e8cd64c690
Y.Yamashiro <yamashiro.yoshihiro@mono.cafe>
2024-01-31 23:13:30 +0900

    Configuration Bitsを仮設定

--- drv/mcal/config_bits.c
+++ drv/mcal/config_bits.c
@@ -1,1 +1,11 @@
+// C:\Program Files\Microchip\xc8\v2.46\pic\dat\cfgdata\16hv753.cfgdata
 
+// CONFIG
+#pragma config FOSC0 = INT     // Oscillator Selection bit
+#pragma config WDTE = ON       // Watchdog Timer Enable bit
+#pragma config PWRTE = ON      // Power-up Timer Enable bit
+#pragma config MCLRE = ON      // MCLR/VPP Pin Function Select bit
+#pragma config CP = ON         // Code Protection bit
+#pragma config BOREN = EN      // Brown-out Reset Enable bits
+#pragma config WRT = ALL       // Flash Program Memory Self Write Enable bit
+#pragma config CLKOUTEN = OFF  // Clock Out Enable bit
