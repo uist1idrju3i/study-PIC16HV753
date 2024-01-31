4a18a2f5504053822cbcdda4e53a96458d583ea9
Y.Yamashiro <yamashiro.yoshihiro@mono.cafe>
2024-02-01 00:43:06 +0900

    AppInitializeを作成

--- main.c
+++ main.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include <xc.h>
 
+#include "app/initialize.h"
 #include "lib/function.h"
 #include "lib/infinite_loop.h"
 
 int main(void) {
+  // Initialize
+  while (kSuccess != app_initialize()) {
+    lib_infinite_loop();
+  }
+
+  // Main loop
   while (1) {
   }
-  lib_infinite_loop();
+
   exit(EXIT_SUCCESS);
 }
